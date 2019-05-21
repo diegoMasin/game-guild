f82d7b762b0249de91118a99c89247b9679d0cce
Diego Masin <diego.masin@defensoria.ce.def.br>
2019-05-21 17:06:26 -0300

    Criando Grupo.

--- bdo_gestor_guilda/core/forms/grupos.py
+++ bdo_gestor_guilda/core/forms/grupos.py
@@ -6,14 +6,12 @@ from bdo_gestor_guilda.core.models.grupos import Grupos
 class GruposForm(forms.ModelForm):
     class Meta:
         model = Grupos
-        fields = ['lider', 'titulo', 'tipo_guerra']
+        fields = ['lider', 'titulo']
         labels = {
             'lider': 'Líder da Party',
             'titulo': 'Título da Party',
-            'tipo_guerra': 'Tipo de Guerra',
         }
         widgets = {
             'lider': forms.Select(attrs={'class': 'form-control select2', 'required': True}),
             'titulo': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
-            'tipo_guerra': forms.Select(attrs={'class': 'form-control'}),
         }
