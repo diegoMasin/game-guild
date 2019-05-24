cbabc0f01a959413d5cacb06fdc24d32efbad5d7
Diego Masin <diego.masin@defensoria.ce.def.br>
2019-05-24 16:58:16 -0300

    Iniciando Anuncios Gerais.

---
+++ bdo_gestor_guilda/core/views/anuncios_gerais.py
@@ -0,0 +1,55 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+
+from bdo_gestor_guilda.core.forms.anuncios_gerais import AnunciosGeraisForm
+from bdo_gestor_guilda.core.helpers import utils
+from bdo_gestor_guilda.core.helpers.default_texts import TextosPadroes
+from bdo_gestor_guilda.core.models.anuncios_gerais import AnunciosGerais
+
+
+@login_required
+def cadastrar(request):
+    context = utils.get_context(request)
+    context.update({'form': AnunciosGeraisForm()})
+    return render(request, '{0}/cadastrar.html'.format(utils.path_anuncios_gerais), context)
+
+
+# @login_required
+# def inserir(request):
+#     try:
+#         if request.method == 'POST':
+#             form = VinculoGruposForm(request.POST)
+#             if form.is_valid():
+#                 dados = form.cleaned_data
+#                 membro = dados.get('membro')
+#                 is_lider_grupo = Grupos.objects.filter(lider=membro).count() > 0
+#                 is_membro_grupo = VinculoGrupos.objects.filter(membro=membro).count() > 0
+#                 if not is_lider_grupo and not is_membro_grupo:
+#                     VinculoGrupos(**dados).save()
+#                     messages.success(request, TextosPadroes.salvar_sucesso_o('Vinculo'))
+#                 else:
+#                     messages.error(request, '{} já é líder ou membro em outro grupo fixo.'.format(membro))
+#                     return redirect(utils.url_vinculo_grupos_cadastrar)
+#             else:
+#                 erros_form = utils.TextosPadroes.errors_form(form)
+#                 for error in erros_form:
+#                     messages.warning(request, error)
+#                 return redirect(utils.url_vinculo_grupos_cadastrar)
+#     except Exception as e:
+#         messages.warning(request, TextosPadroes.erro_padrao())
+#         return redirect(utils.url_vinculo_grupos_cadastrar)
+#     return redirect(utils.url_grupos_listar)
+#
+#
+# @login_required
+# def deletar(request, vinculo_grupo_id):
+#     try:
+#         context = utils.get_context(request)
+#         membro = VinculoGrupos.objects.get(pk=vinculo_grupo_id)
+#         membro.delete()
+#         messages.success(request, '{0} Removido com Sucesso!'.format(membro))
+#     except Exception as e:
+#         messages.error(request, utils.TextosPadroes.erro_padrao())
+#     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
