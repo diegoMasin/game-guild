684cd48b4a0a31b8f0f9a9c4f74dd615a76fc18b
Diego Masin <diegoifce@gmail.com>
2019-06-30 20:31:54 -0300

    Modificando mais o bot.

--- bdo_gestor_guilda/core/bot.py
+++ bdo_gestor_guilda/core/bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import asyncio
 
 client = discord.Client()
-guild = client.get_guild(id=593449968648585236)
+guild = client.get_guild(id=413677493204156416)
 
 @client.event
 async def on_ready():
@@ -13,10 +13,10 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    guild = client.get_guild(593449968648585236)
+    guild = client.get_guild(413677493204156416)
     nodewar = guild.get_channel(520001040289103903)
     if message.content == '!nodewar':
-        members =[m.name for m in nodewar.members]
+        members = ['{}#{}'.format(m._user.display_name, m._user.discriminator) for m in nodewar.members]
         await message.channel.send(members)
 
 client.run('NTkzNDQ5OTY4NjQ4NTg1MjM2.XRURrA.oFsjb1AYZySz_Us-JWZ7I3kgxpE')
