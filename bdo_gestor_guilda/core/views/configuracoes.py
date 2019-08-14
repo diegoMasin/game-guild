7ebced69ad89c8c3b4302aa5f1dc3d32a05c7d54
Diego Masin <diego.masin@defensoria.ce.def.br>
2019-08-14 14:51:26 -0300

    Finalizada exibição da Limpeza de registros. Calculando numeros e %.

--- bdo_gestor_guilda/core/views/configuracoes.py
+++ bdo_gestor_guilda/core/views/configuracoes.py
@@ -8,14 +8,24 @@ from bdo_gestor_guilda.core.forms.guerras import GuerrasForm
 from bdo_gestor_guilda.core.helpers import utils
 from bdo_gestor_guilda.core.helpers.default_texts import TextosPadroes
 from bdo_gestor_guilda.core.models.configuracoes import Configuracoes
+from bdo_gestor_guilda.core.models.guerras import Guerras
+from bdo_gestor_guilda.core.models.participar_guerra import ParticiparGuerra
+from bdo_gestor_guilda.core.models.frequencia_guerra import FrequenciaGuerra
+from bdo_gestor_guilda.core.models.payout import Payout
+from bdo_gestor_guilda.core.models.payout_personalizado import PayoutPersonalizado
+from bdo_gestor_guilda.usuario.models.user_avancado import UserAvancado
 
 
 @login_required
 def index(request):
     context = utils.get_context(request)
     if context.get('dados_avancados').is_lider():
+        num_registros, percent = contador_de_registros(request)
         todas_configuracoes = Configuracoes.objects.all().order_by('pk')
+
         context.update({'todas_configuracoes': todas_configuracoes})
+        context.update({'num_registros': num_registros})
+        context.update({'percent': percent})
         return render(request, '{0}/index.html'.format(utils.path_configuracoes), context)
     return redirect(utils.url_name_home)
 
@@ -50,4 +60,18 @@ def validacao_geral(conf):
 
 
 def contador_de_registros(request):
-    return 0
+    start_de_seguranca = 500
+    num_registros = start_de_seguranca
+    reg_guerras = Guerras.objects.all().count()
+    reg_participacoes = ParticiparGuerra.objects.all().count()
+    reg_frequencias = FrequenciaGuerra.objects.all().count()
+    reg_payout = Payout.objects.all().count()
+    reg_payout_personalizado = PayoutPersonalizado.objects.all().count()
+    reg_black_list = UserAvancado.objects.filter(ativo=False, cargo=UserAvancado.CARGO_NENHUM_ID).count()
+
+    soma = reg_guerras + reg_participacoes + reg_frequencias + reg_payout + reg_payout_personalizado + reg_black_list
+    num_registros = num_registros + soma
+    if soma == 0:
+        num_registros = 0
+    percent = (num_registros * 100) / 10000
+    return num_registros, percent
